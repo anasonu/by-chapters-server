5828f6691199e60bff30bd354b51165f0e308ddb
Ana Sobrino <anasonu@hotmail.com>
2022-06-02 18:59:56 +0200

    Add login and token verification

--- error-handling/index.js
+++ error-handling/index.js
@@ -9,6 +9,12 @@ module.exports = (app) => {
     // always logs the error
     console.error("ERROR", req.method, req.path, err);
 
+    if(err.status === 401)  {
+      res.status(401).json({
+        errorMessage: "Credenciales no validadas",
+      });
+    };
+
     // only render if the error ocurred before sending the response
     if (!res.headersSent) {
       res
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const AuthorModel = require("../models/Author.Model.js");
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+const isLoggedIn = require("../middlewares/isLoggedIn.js");
+
 
 // POST "/api/auth/signup" => Registrar un usuario
 router.post("/signup", async (req, res, next) => {
@@ -59,6 +62,56 @@ router.post("/signup", async (req, res, next) => {
 });
 
 
+// POST ("/api/auth/login") => Verificar credenciales del usuario y darle acceso
+router.post("/login", async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+
+        const foundAuthor = await AuthorModel.findOne({ email });
+        if(foundAuthor === null) {
+            res.status(400).json({
+                errorMessage: "El usuario no está registrado",
+            });
+            return;
+        }
+
+        const passwordMatch = await bcrypt.compare(password, foundAuthor.password);
+        if(passwordMatch === false) {
+            res.status(401).json({
+                errorMessage: "La contraseña es incorrecta",
+            });
+            return;
+        }
+
+        const payload = {
+            _id: foundAuthor._id,
+            email: foundAuthor.email,
+            username: foundAuthor.username,
+            name: foundAuthor.name,
+            lastName: foundAuthor.lastName,
+            admin: foundAuthor.admin,
+        };
+
+        const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
+            algorithm: "HS256",
+            expiresIn: "12h",
+        });
+
+        console.log(authToken)
+        res.json({ authToken });
+        
+    } catch (error) {
+        next(error);
+    };
+
+});
+
+
+// GET "api/auth/verify" => Verificar token
+router.get("/verify", isLoggedIn, async (req, res, next) => {
+    res.json(req.payload);
+});
 
 
 module.exports = router;
